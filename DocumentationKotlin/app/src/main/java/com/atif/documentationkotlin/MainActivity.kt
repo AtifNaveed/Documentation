17dee16df793a276adf5847b5a0a65f7deaa2424
“Atif <atif.gcucs@gmail.com>
2019-09-26 10:26:46 +0400

    Documentation

---
+++ DocumentationKotlin/app/src/main/java/com/atif/documentationkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.atif.documentationkotlin
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * - Override onCreate logic here
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
